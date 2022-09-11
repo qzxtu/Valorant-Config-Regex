231cf157930e8207775068d95dbe6ecf1bd795aa
Nova <69091361+qzxtu@users.noreply.github.com>
2022-09-10 19:02:58 -0500

    Add files via upload

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string rootPath = Path.Combine(folder, "Valorant\\Saved\\Config");
+            string[] dirs = Directory.GetDirectories(rootPath, "*", SearchOption.TopDirectoryOnly);
+            foreach (string dir in dirs)
+            {
+                var a = Regex.Match(dir, @"(.+?)-latam").Groups[1].Value;
+                Console.WriteLine(a);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
